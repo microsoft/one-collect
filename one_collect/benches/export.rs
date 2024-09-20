661a115ee7b46e7966b1fee0b3e1e3750f477832
Beau Belgrave (HE/HIM) <beaub@linux.microsoft.com>
2024-09-20 12:07:46 -0700

    Bench: Add bench for exporting
    We need a benchmark for parts of exporting that are performance
    critical. Now that we added time to module lookup and built a linear and
    logarithmic approach, we should measure them.
    
    Add small/large module lookups for both linear and log based find cases.
    
    Signed-off-by: Beau Belgrave (HE/HIM) <beaub@linux.microsoft.com>

---
+++ one_collect/benches/export.rs
@@ -0,0 +1,58 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use one_collect::helpers::exporting::*;
+use crate::mappings::ExportMappingLookup;
+
+fn new_map(
+    time: u64,
+    start: u64,
+    end: u64,
+    id: usize) -> ExportMapping {
+    ExportMapping::new(time, 0, start, end, 0, false, id)
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut lookup = ExportMappingLookup::default();
+
+    for i in 0..16 {
+        let start = i * 1024;
+        let end = start + 1023;
+
+        lookup.mappings_mut().push(new_map(0, start, end, i as usize));
+    }
+
+    lookup.set_lookup_min_size(usize::MAX);
+
+    c.bench_function("small (linear)", |b| b.iter(|| {
+        lookup.find(0, None).unwrap();
+    }));
+
+    lookup.set_lookup_min_size(0);
+
+    c.bench_function("small (logarithmic)", |b| b.iter(|| {
+        lookup.find(0, None).unwrap();
+    }));
+
+    lookup.mappings_mut().clear();
+
+    for i in 0..128 {
+        let start = i * 1024;
+        let end = start + 1023;
+
+        lookup.mappings_mut().push(new_map(0, start, end, i as usize));
+    }
+
+    lookup.set_lookup_min_size(usize::MAX);
+
+    c.bench_function("large (linear)", |b| b.iter(|| {
+        lookup.find(0, None).unwrap();
+    }));
+
+    lookup.set_lookup_min_size(0);
+
+    c.bench_function("large (logarithmic)", |b| b.iter(|| {
+        lookup.find(0, None).unwrap();
+    }));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
