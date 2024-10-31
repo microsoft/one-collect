d73c2fb9f34a5e68a8f7e4625bd141b2fa3f61b6
Brian Robbins <brianrob@microsoft.com>
2024-10-31 10:56:54 -0700

    Implement PE Image Reader Capabilities (#93)
    * Implement PE image reader capabilities.
    
    PE images are used as the envelope for executable code on Windows and
    executable managed code on non-Windows. Resolving symbols in this code
    requires the ability to open the PE file and access metadata used to
    lookup and validate matching symbol files (PDB and PerfMap).
    
    ---------
    
    Co-authored-by: Brian Robbins <brianrob@linux.microsoft.com>

--- one_collect/src/helpers/mod.rs
+++ one_collect/src/helpers/mod.rs
@@ -1,6 +1,7 @@
 pub mod callstack;
 pub mod dotnet;
 pub mod exporting;
+pub mod modules;
 
 #[cfg(target_os = "linux")]
 pub mod uprobe;
---
+++ one_collect/src/helpers/modules/pe_file.rs
@@ -0,0 +1,662 @@
+use std::io::{Read, Seek, SeekFrom};
+use std::fs::File;
+use std::error::Error;
+use std::mem::{zeroed, size_of};
+use std::string::{FromUtf8Error, FromUtf16Error};
+use std::slice;
+
+#[derive(Default)]
+pub struct PEModuleInfo {
+    pub machine: u16,
+    pub date_time: u32,
+    pub symbol_name: Option<String>,
+    pub symbol_age: u32,
+    pub symbol_sig: [u8; 16],
+    pub version_name: Option<String>,
+    pub perfmap_sig: [u8; 16],
+    pub perfmap_ver: u32,
+    pub perfmap_name: Option<String>,
+}
+
+impl PEModuleInfo {
+    pub fn reset(
+        &mut self) {
+        self.machine = 0;
+        self.date_time = 0;
+        self.symbol_name = None;
+        self.symbol_age = 0;
+        self.symbol_sig = [0; 16];
+        self.version_name = None;
+        self.perfmap_sig = [0; 16];
+        self.perfmap_ver = 0;
+        self.perfmap_name = None;
+    }
+
+    pub fn get_info(
+        &mut self,
+        path: &str) -> anyhow::Result<()> {
+        let file = File::open(path)?;
+        self.get_info_direct(
+            file)
+    }
+
+    pub fn get_info_direct(
+        &mut self,
+        mut file: File) -> anyhow::Result<()> {
+        get_pe_info(&mut file, self)?;
+
+        Ok(())
+    }
+}
+
+
+#[repr(C)]
+struct PEHeader {
+    signature: u32,
+    machine: u16,
+    sec_count: u16,
+    date_time: u32,
+    sym_table: u32,
+    sym_count: u32,
+    opt_header_size: u16,
+    characteristics: u16,
+    magic: u16,
+}
+
+#[repr(C)]
+struct PEDataDirectory {
+    virt_addr: u32,
+    size: u32,
+}
+
+#[repr(C)]
+struct PEDebugDirectory {
+    reserved: u32,
+    date_time: u32,
+    major_ver: u16,
+    minor_ver: u16,
+    debug_type: u32,
+    size: u32,
+    virt_addr: u32,
+    raw_offset: u32,
+}
+
+#[repr(C)]
+struct PEResDirectory {
+    flags: u32,
+    date_time: u32,
+    major_ver: u16,
+    minor_ver: u16,
+    named_count: u16,
+    id_count: u16,
+}
+
+#[repr(C)]
+struct PEResEntry {
+    name_id_offset: u32,
+    dir_data_offset: u32,
+}
+
+#[repr(C)]
+struct PEResData {
+    data_offset: u32,
+    size: u32,
+    code_page: u32,
+    reserved: u32,
+}
+
+impl PEResEntry {
+    fn is_named(&self) -> bool {
+        (self.name_id_offset & 0x80000000) != 0
+    }
+
+    fn name_id_value(&self) -> u32 {
+        self.name_id_offset & 0x7FFFFFFF
+    }
+
+    fn is_dir(&self) -> bool {
+        (self.dir_data_offset & 0x80000000) != 0
+    }
+
+    fn dir_data_value(&self) -> u32 {
+        self.dir_data_offset & 0x7FFFFFFF
+    }
+}
+
+#[repr(C)]
+struct PESection {
+    name: u64,
+    virt_size: u32,
+    virt_addr: u32,
+    raw_size: u32,
+    raw_offset: u32,
+    rel_offset: u32,
+    line_offset: u32,
+    rel_count: u16,
+    line_count: u16,
+    characteristics: u32,
+}
+
+#[repr(C)]
+struct CodeViewNb10 {
+    offset: u32,
+    pdb_sig: [u8; 4],
+    pdb_age: u32,
+    pdb_name: [u8; 128],
+}
+
+#[repr(C)]
+struct CodeViewRsds {
+    pdb_sig: [u8; 16],
+    pdb_age: u32,
+    pdb_name: [u8; 128],
+}
+
+const PERFMAP_MAGIC: u32 = 0x4D523252;
+
+#[derive(Debug)]
+#[repr(C)]
+struct CodeViewPerfMap {
+    perfmap_magic: u32,
+    perfmap_sig: [u8; 16],
+    perfmap_ver: u32,
+    perfmap_name: [u8; 128],
+}
+
+fn read_header(
+    reader: &mut impl Read,
+    header: &mut PEHeader) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                header as *mut _ as *mut u8,
+                size_of::<PEHeader>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_directory(
+    reader: &mut impl Read,
+    dir: &mut PEDataDirectory) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                dir as *mut _ as *mut u8,
+                size_of::<PEDataDirectory>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_cv_nb10(
+    reader: &mut impl Read,
+    dir: &mut CodeViewNb10) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                dir as *mut _ as *mut u8,
+                size_of::<CodeViewNb10>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_cv_rsds(
+    reader: &mut impl Read,
+    dir: &mut CodeViewRsds) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                dir as *mut _ as *mut u8,
+                size_of::<CodeViewRsds>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_cv_perfmap(
+    reader: &mut impl Read,
+    dir: &mut CodeViewPerfMap) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                dir as *mut _ as *mut u8,
+                size_of::<CodeViewPerfMap>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_debug_directory(
+    reader: &mut impl Read,
+    dir: &mut PEDebugDirectory) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                dir as *mut _ as *mut u8,
+                size_of::<PEDebugDirectory>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_res_directory(
+    reader: &mut impl Read,
+    dir: &mut PEResDirectory) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                dir as *mut _ as *mut u8,
+                size_of::<PEResDirectory>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_res_entry(
+    reader: &mut impl Read,
+    entry: &mut PEResEntry) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                entry as *mut _ as *mut u8,
+                size_of::<PEResEntry>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_res_data(
+    reader: &mut impl Read,
+    entry: &mut PEResData) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                entry as *mut _ as *mut u8,
+                size_of::<PEResData>()))?;
+    }
+
+    Ok(())
+}
+
+fn find_res_entry(
+    reader: &mut (impl Read + Seek),
+    offset: u64,
+    wanted_id: u32) -> anyhow::Result<Option<PEResEntry>> {
+    let mut res_dir: PEResDirectory = unsafe { zeroed() };
+    let mut entry: PEResEntry = unsafe { zeroed() };
+    reader.seek(SeekFrom::Start(offset))?;
+    read_res_directory(reader, &mut res_dir)?;
+    let count = res_dir.named_count + res_dir.id_count;
+
+    for _i in 0 .. count {
+        read_res_entry(reader, &mut entry)?;
+
+        if !entry.is_named() {
+            let id = entry.name_id_value() & 0xFFFF;
+
+            if id == wanted_id || wanted_id == 0 {
+                return Ok(Some(entry));
+            }
+        }
+    }
+
+    Ok(None)
+}
+
+fn read_section(
+    reader: &mut impl Read,
+    sec: &mut PESection) -> anyhow::Result<()> {
+    unsafe {
+        reader.read_exact(
+            slice::from_raw_parts_mut(
+                sec as *mut _ as *mut u8,
+                size_of::<PESection>()))?;
+    }
+
+    Ok(())
+}
+
+fn read_u32(
+    reader: &mut impl Read) -> anyhow::Result<u32> {
+    let mut v: [u8; 4] = [0; 4];
+    reader.read_exact(&mut v)?;
+    Ok(u32::from_le_bytes(v))
+}
+
+fn get_pe_header(
+    reader: &mut (impl Read + Seek),
+    pe_header: &mut PEHeader,
+    pe_offset: &mut u64) -> anyhow::Result<()> {
+    /* Get offset to pe header */
+    reader.seek(SeekFrom::Start(0x3C))?;
+    *pe_offset = read_u32(reader)? as u64;
+
+    /* Read PE header */
+    reader.seek(SeekFrom::Start(*pe_offset))?;
+    read_header(reader, pe_header)?;
+
+    Ok(())
+}
+
+fn va_to_offset(
+    reader: &mut (impl Read + Seek),
+    pe_header: &PEHeader,
+    pe_offset: u64,
+    va: u32) -> anyhow::Result<Option<u64>> {
+    /* Read sections */
+    let mut sections_offset: u64 = pe_offset as u64;
+    sections_offset += 24;
+    sections_offset += pe_header.opt_header_size as u64;
+    reader.seek(SeekFrom::Start(sections_offset))?;
+    let mut section: PESection = unsafe { zeroed() };
+
+    for _i in 0 .. pe_header.sec_count {
+        read_section(reader, &mut section)?;
+        let va_start = section.virt_addr;
+        let va_end = va_start + section.raw_size;
+
+        /* Check if within virtual address range */
+        if va >= va_start &&
+           va < va_end {
+            let rva = va - va_start;
+            return Ok(Some((rva + section.raw_offset) as u64));
+        }
+    }
+
+    return Ok(None)
+}
+
+fn get_directory_data(
+    reader: &mut (impl Read + Seek),
+    pe_header: &PEHeader,
+    pe_offset: u64,
+    index: u64) -> anyhow::Result<(u64, u64)> {
+    /* Get data header offset */
+    let data_offset: u64;
+
+    if pe_header.magic == 0x10b {
+        data_offset = pe_offset + 24 + 96 + (index * 8);
+    } else if pe_header.magic == 0x20b {
+        data_offset = pe_offset + 24 + 112 + (index * 8);
+    } else {
+        return Err(anyhow::Error::msg(
+            format!(
+                "Unexpected magic value {}",
+                pe_header.magic)));
+    }
+
+    /* Read data directory */
+    reader.seek(SeekFrom::Start(data_offset))?;
+    let mut data_dir: PEDataDirectory = unsafe { zeroed() };
+    read_directory(reader, &mut data_dir)?;
+
+    /* Not there */
+    if data_dir.virt_addr == 0 {
+        return Ok((0, 0));
+    }
+
+    /* Read sections */
+    let mut sections_offset: u64 = pe_offset as u64;
+    sections_offset += 24;
+    sections_offset += pe_header.opt_header_size as u64;
+    reader.seek(SeekFrom::Start(sections_offset))?;
+    let mut section: PESection = unsafe { zeroed() };
+    let mut data_offset: u64 = 0;
+
+    for _i in 0 .. pe_header.sec_count {
+        read_section(reader, &mut section)?;
+        let va_start = section.virt_addr;
+        let va_end = va_start + section.raw_size;
+
+        /* Check if within virtual address range */
+        if data_dir.virt_addr >= va_start &&
+           data_dir.virt_addr < va_end {
+            let rva = data_dir.virt_addr - va_start;
+            data_offset = (rva + section.raw_offset) as u64;
+            break;
+        }
+    }
+
+    Ok((data_offset, data_dir.size as u64))
+}
+
+fn get_unicode_string(
+    name: &[u8]) -> Result<String, FromUtf16Error> {
+    unsafe {
+        let (_, values, _) = name.align_to::<u16>();
+        let null_pos = values.iter()
+            .position(|&c| c == 0u16)
+            .unwrap_or(values.len());
+
+        String::from_utf16(&values[0..null_pos])
+    }
+}
+
+fn get_string(
+    name: &[u8]) -> Result<String, FromUtf8Error> {
+    let null_pos = name.iter()
+        .position(|&c| c == b'\0')
+        .unwrap_or(name.len());
+
+    String::from_utf8((&name[0..null_pos]).to_vec())
+}
+
+fn get_pe_info(
+    reader: &mut (impl Read + Seek),
+    module: &mut PEModuleInfo) -> anyhow::Result<()> {
+    let dbg_offset: u64;
+    let dbg_size: u64;
+    let res_offset: u64;
+    let mut pe_header: PEHeader = unsafe { zeroed() };
+    let mut pe_offset: u64 = 0;
+
+    module.machine = 0;
+    module.date_time = 0;
+    module.symbol_name = None;
+    module.symbol_age = 0;
+    module.symbol_sig = [0; 16];
+    module.version_name = None;
+
+    get_pe_header(reader, &mut pe_header, &mut pe_offset)?;
+    module.machine = pe_header.machine;
+    module.date_time = pe_header.date_time;
+
+    (dbg_offset, dbg_size) = get_directory_data(
+        reader, &pe_header, pe_offset, 6)?;
+
+    if dbg_offset != 0 {
+        let mut debug_dir: PEDebugDirectory = unsafe { zeroed() };
+        reader.seek(SeekFrom::Start(dbg_offset))?;
+        let debug_count = dbg_size / size_of::<PEDebugDirectory>() as u64;
+
+        for i in 0 .. debug_count {
+            reader.seek(SeekFrom::Start(dbg_offset + (i * size_of::<PEDebugDirectory>() as u64)))?;
+            read_debug_directory(reader, &mut debug_dir)?;
+
+            /* Codeview */
+            if debug_dir.debug_type == 2 {
+                reader.seek(SeekFrom::Start(debug_dir.raw_offset as u64))?;
+                let cv_type = read_u32(reader)?;
+
+                if cv_type == 0x3031424e {
+                    /* NB10 */
+                    let mut cv: CodeViewNb10 = unsafe { zeroed() };
+                    read_cv_nb10(reader, &mut cv)?;
+                    module.symbol_age = cv.pdb_age;
+                    module.symbol_sig[0..4].clone_from_slice(&cv.pdb_sig);
+                    module.symbol_name = Some(get_string(&cv.pdb_name)?);
+                } else if cv_type == 0x53445352 {
+                    /* RSDS */
+                    let mut cv: CodeViewRsds = unsafe { zeroed() };
+                    read_cv_rsds(reader, &mut cv)?;
+                    module.symbol_age = cv.pdb_age;
+                    module.symbol_sig[0..16].clone_from_slice(&cv.pdb_sig);
+                    module.symbol_name = Some(get_string(&cv.pdb_name)?);
+                }
+            }
+            /* PerfMap */
+            else if debug_dir.debug_type == 21 && debug_dir.major_ver == 1 && debug_dir.minor_ver == 0 {
+                reader.seek(SeekFrom::Start(debug_dir.raw_offset as u64))?;
+
+                let mut cv: CodeViewPerfMap = unsafe { zeroed() };
+                read_cv_perfmap(reader, &mut cv)?;
+                if cv.perfmap_magic == PERFMAP_MAGIC {
+                    module.perfmap_sig[0..16].clone_from_slice(&cv.perfmap_sig);
+                    module.perfmap_ver = cv.perfmap_ver;
+                    module.perfmap_name = Some(get_string(&cv.perfmap_name)?);
+                }
+            }
+        }
+    }
+
+    /* Get file version information */
+    (res_offset, _) = get_directory_data(
+        reader, &pe_header, pe_offset, 2)?;
+
+    if res_offset != 0 {
+        // Locate directory tree: 16 / 1 / 0
+        let mut cont: bool = false;
+        let mut offset: u64 = 0;
+
+        if let Some(entry) = find_res_entry(reader, res_offset + offset, 16)? {
+            if entry.is_dir() {
+                offset = entry.dir_data_value() as u64;
+                cont = true;
+            }
+        }
+
+        if cont {
+            cont = false;
+            if let Some(entry) = find_res_entry(reader, res_offset + offset, 1)? {
+                if entry.is_dir() {
+                    offset = entry.dir_data_value() as u64;
+                    cont = true;
+                }
+            }
+        }
+
+        if cont {
+            if let Some(entry) = find_res_entry(reader, res_offset + offset, 0)? {
+                let mut data: PEResData = unsafe { zeroed() };
+                offset = entry.dir_data_value() as u64;
+                reader.seek(SeekFrom::Start(res_offset + offset))?;
+                read_res_data(reader, &mut data)?;
+
+                /* Resource offset is a VA within the binary */
+                if let Some(data_offset) = va_to_offset(
+                    reader,
+                    &pe_header,
+                    pe_offset,
+                    data.data_offset)? {
+                    /* Read data at found file offset */
+                    let mut block_array = [0u8; 4096];
+                    let block_size = std::cmp::min(data.size, 4096);
+                    let mut slice = &mut block_array[..block_size as usize];
+                    reader.seek(SeekFrom::Start(data_offset))?;
+                    reader.read_exact(&mut slice)?;
+
+                    let mut product: Option<String> = None;
+                    let mut file_ver: Option<String> = None;
+                    let mut file_desc: Option<String> = None;
+
+                    while slice.len() > 8 {
+                        /*let len = u16::from_ne_bytes(slice[0..2].try_into().unwrap());*/
+                        let mut data_len = u16::from_ne_bytes(slice[2..4].try_into().unwrap());
+                        let data_type = u16::from_ne_bytes(slice[4..6].try_into().unwrap());
+
+                        /* Data is string */
+                        if data_type == 1 {
+                            /* Characters vs bytes */
+                            data_len *= 2;
+                        }
+
+                        let name = get_unicode_string(&slice[6..])?;
+                        let header_len = 8 + (name.len() * 2);
+                        /* Align header */
+                        let header_len = (header_len + 3) & !3;
+
+                        match name.as_str() {
+                            "ProductVersion" => {
+                                if product.is_none() {
+                                    product = Some(
+                                        get_unicode_string(
+                                            &slice[header_len..])?);
+                                }
+                            },
+                            "FileVersion" => {
+                                if file_ver.is_none() {
+                                    file_ver = Some(
+                                        get_unicode_string(
+                                            &slice[header_len..])?);
+                                }
+                            },
+                            "FileDescription" => {
+                                if file_desc.is_none() {
+                                    file_desc = Some(
+                                        get_unicode_string(
+                                            &slice[header_len..])?);
+                                }
+                            },
+                            _ => {},
+                        }
+
+                        let total_len = header_len + data_len as usize;
+                        /* Must align data */
+                        let total_len = (total_len + 3) & !3;
+
+                        /* No more data */
+                        if slice.len() <= total_len {
+                            break;
+                        }
+
+                        /* No more to get */
+                        if product.is_some() &&
+                           file_ver.is_some() &&
+                           file_desc.is_some() {
+                           break;
+                        }
+
+                        slice = &mut slice[total_len..];
+                    }
+
+                    /* Must have a file description */
+                    if file_desc.is_some() {
+                        /* Prefer product version to file version */
+                        if product.is_some() {
+                            module.version_name = Some(format!("{}, {}",
+                                product.unwrap(),
+                                file_desc.unwrap()));
+                        } else if file_ver.is_some() {
+                            module.version_name = Some(format!("{}, {}",
+                                file_ver.unwrap(),
+                                file_desc.unwrap()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[cfg(target_os = "windows")]
+    #[test]
+    fn get_details() {
+        let mut m = PEModuleInfo::default();
+
+        let windir = env::var("WINDIR").unwrap();
+        let ntdll_path = format!("{}\\System32\\ntdll.dll", windir);
+        for _i in 0..1024 {
+            let mut f = File::open(&ntdll_path).unwrap();
+            get_pe_info(&mut f, &mut m).unwrap();
+        }
+    }
+}
