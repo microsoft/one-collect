8ab05485e62a5e5bb07c385308550485810ef3b6
Brian Robbins <brianrob@microsoft.com>
2025-02-11 15:35:13 -0800

    Emit NetTrace Files from Record-Trace (#122)
    * Record-Trace to NetTrace.
    
    Introduce the --format argument to allow users to specify the output
    format of the trace. Valid values are perfview-xml and nettrace.
    Defaults to nettrace.
    
    * Clean-up Exporter Implementation.
    
     - Allow exporters to validate command line args before capture.
     - Use anyhow to expose errors in exporters.
     - Push output path validation into exporters.
    
    ---------
    
    Co-authored-by: Brian Robbins <brianrob@linux.microsoft.com>

--- record-trace/src/commandline.rs
+++ record-trace/src/commandline.rs
@@ -1,13 +1,19 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use std::env;
+use std::fmt;
 use std::path::PathBuf;
 use std::process;
+
+use crate::export::{Exporter, NetTraceExporter, PerfViewExporter};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(long, help = "Output directory")]
     out: Option<String>,
+
+    #[arg(long, default_value_t = Format::Nettrace, help = "Output format")]
+    format: Format,
 
     #[arg(long, help = "Capture CPU samples")]
     on_cpu: bool,
@@ -19,9 +25,25 @@ struct Args {
     target_pids: Option<Vec<i32>>,
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Format {
+    Nettrace,
+    PerfviewXML,
+}
+
+impl fmt::Display for Format {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Format::Nettrace => write!(f, "nettrace"),
+            Format::PerfviewXML => write!(f, "perfview-xml"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub (crate) struct RecordArgs {
     output_path: PathBuf,
+    format: Format,
     on_cpu: bool,
     off_cpu: bool,
     target_pids: Option<Vec<i32>>,
@@ -33,19 +55,7 @@ impl RecordArgs {
         
         // If --out isn't specified, default to the current working directory.
         let output_path = match command_args.out {
-            Some(path) => {
-                let path = PathBuf::from(path);
-                if path.exists() && !path.is_dir() {
-                    eprintln!("{} is not a directory.", path.display());
-                    process::exit(1);
-                }
-                else if !path.exists() {
-                    eprintln!("{} does not exist.", path.display());
-                    process::exit(1);
-                }
-
-                path
-            },
+            Some(path) => { PathBuf::from(path) },
             None => {
                 match env::current_dir() {
                     Ok(current_dir) => current_dir,
@@ -56,13 +66,14 @@ impl RecordArgs {
 
         let args = Self {
             output_path,
+            format: command_args.format,
             on_cpu: command_args.on_cpu,
             off_cpu: command_args.off_cpu,
             target_pids: command_args.target_pids,
         };
 
         // Cross-argument validation.
-        if !args.on_cpu {
+        if !args.on_cpu && !args.off_cpu {
             eprintln!("No events selected. Exiting.");
             process::exit(1);
         }
@@ -72,6 +83,13 @@ impl RecordArgs {
 
     pub (crate) fn output_path(&self) -> &PathBuf {
         &self.output_path
+    }
+
+    pub (crate) fn format(&self) -> Box<dyn Exporter> {
+        match self.format {
+            Format::Nettrace => Box::new(NetTraceExporter::new()),
+            Format::PerfviewXML => Box::new(PerfViewExporter::new()),
+        }
     }
 
     pub (crate) fn on_cpu(&self) -> bool {
--- record-trace/src/export.rs
+++ record-trace/src/export.rs
@@ -1,27 +1,77 @@
 use one_collect::helpers::exporting::ExportMachine;
+use one_collect::helpers::exporting::formats::nettrace::*;
 use one_collect::helpers::exporting::formats::perf_view::*;
 use one_collect::helpers::exporting::graph::ExportGraph;
 
 use crate::commandline::RecordArgs;
+use anyhow::anyhow;
+use std::path::PathBuf;
 
 pub (crate) trait Exporter {
+    fn validate(
+        &mut self,
+        args: &RecordArgs) -> anyhow::Result<()>;
+
     fn run(
-        machine: &ExportMachine,
-        args: &RecordArgs) -> bool;
+        &self,
+        machine: &mut ExportMachine,
+        args: &RecordArgs) -> anyhow::Result<()>;
 }
 
 pub (crate) struct PerfViewExporter {
 }
 
+impl PerfViewExporter {
+    pub fn new() -> Self {
+        Self {
+        }
+    }
+}
+
 impl Exporter for PerfViewExporter {
+    fn validate(
+        &mut self,
+        args: &RecordArgs) -> anyhow::Result<()> {
+        let output_path = args.output_path();
+        if output_path.exists() && !output_path.is_dir() {
+            return Err(anyhow!("{} is not a directory.", output_path.display()));
+        }
+        else if !output_path.exists() {
+            return Err(anyhow!("{} does not exist.", output_path.display()));
+        }
+
+        Ok(())
+    }
+
     fn run(
-        machine: &ExportMachine,
-        args: &RecordArgs) -> bool {
+        &self,
+        machine: &mut ExportMachine,
+        args: &RecordArgs) -> anyhow::Result<()> {
+
         /* Split by comm name */
         let comm_map = machine.split_processes_by_comm();
 
-        let cpu = machine.find_sample_kind("cpu").expect("CPU sample kind should be known.");
-        let cswitch = machine.find_sample_kind("cswitch").expect("CSwitch sample kind should be known.");
+        let cpu = match machine.find_sample_kind("cpu") {
+            Some(cpu) => { cpu },
+            None => {
+                if args.on_cpu() {
+                    return Err(anyhow!("CPU sample kind should be known."));
+                }
+
+                0
+            }
+        };
+
+        let cswitch = match machine.find_sample_kind("cswitch") {
+            Some(cswitch) => { cswitch },
+            None => {
+                if args.off_cpu() {
+                    return Err(anyhow!("CSwitch sample kind should be known."));
+                }
+
+                0
+            }
+        };
 
         let mut graph = ExportGraph::new();
         let mut buf: String;
@@ -32,25 +82,29 @@ impl Exporter for PerfViewExporter {
                     for pid in pids {
                         let single_pid = vec![pid];
 
-                        let path = format!("{}/t.Unknown.{}.CPU.PerfView.xml", args.output_path().display(), pid);
+                        if args.on_cpu() {
+                            let path = format!("{}/t.Unknown.{}.CPU.PerfView.xml", args.output_path().display(), pid);
 
-                        Self::export_pids(
-                            machine,
-                            &mut graph,
-                            &single_pid,
-                            cpu,
-                            &path,
-                            "CPU Samples");
+                            Self::export_pids(
+                                machine,
+                                &mut graph,
+                                &single_pid,
+                                cpu,
+                                &path,
+                                "CPU Samples");
+                        }
 
-                        let path = format!("{}/t.Unknown.{}.CSwitch.PerfView.xml", args.output_path().display(), pid);
+                        if args.off_cpu() {
+                            let path = format!("{}/t.Unknown.{}.CSwitch.PerfView.xml", args.output_path().display(), pid);
 
-                        Self::export_pids(
-                            machine,
-                            &mut graph,
-                            &single_pid,
-                            cswitch,
-                            &path,
-                            "Wait Time");
+                            Self::export_pids(
+                                machine,
+                                &mut graph,
+                                &single_pid,
+                                cswitch,
+                                &path,
+                                "Wait Time");
+                        }
                     }
                 },
                 Some(comm_id) => {
@@ -67,29 +121,33 @@ impl Exporter for PerfViewExporter {
                         Err(_) => { "Unknown" },
                     };
 
-                    let path = format!("{}/t.{}.CPU.PerfView.xml", args.output_path().display(), comm);
+                    if args.on_cpu() {
+                        let path = format!("{}/t.{}.CPU.PerfView.xml", args.output_path().display(), comm);
 
-                    Self::export_pids(
-                        machine,
-                        &mut graph,
-                        &pids,
-                        cpu,
-                        &path,
-                        "CPU Samples");
+                        Self::export_pids(
+                            machine,
+                            &mut graph,
+                            &pids,
+                            cpu,
+                            &path,
+                            "CPU Samples");
+                    }
 
-                    let path = format!("{}/t.{}.CSwitch.PerfView.xml", args.output_path().display(), comm);
+                    if args.off_cpu() {
+                        let path = format!("{}/t.{}.CSwitch.PerfView.xml", args.output_path().display(), comm);
 
-                    Self::export_pids(
-                        machine,
-                        &mut graph,
-                        &pids,
-                        cswitch,
-                        &path,
-                        "Wait Time");
+                        Self::export_pids(
+                            machine,
+                            &mut graph,
+                            &pids,
+                            cswitch,
+                            &path,
+                            "Wait Time");
+                    }
                 }
             }
         }
-    true
+        Ok(())
     }
 }
 
@@ -119,5 +177,49 @@ impl PerfViewExporter {
 
             println!("{}: {} {}", path, total, sample_desc);
         }
+    }
+}
+
+pub (crate) struct NetTraceExporter {
+    output_path: PathBuf,
+}
+
+impl NetTraceExporter {
+    pub fn new() -> Self {
+        Self {
+            output_path: PathBuf::new(),
+        }
+    }
+}
+
+impl Exporter for NetTraceExporter {
+    fn validate(
+        &mut self,
+        args: &RecordArgs) -> anyhow::Result<()> {
+        let output_path = args.output_path();
+        self.output_path.push(args.output_path());
+
+        if output_path.exists() && output_path.is_dir() {
+            if let Some(extension) = output_path.extension() {
+                if extension == "nettrace" {
+                    return Err(anyhow!("{} is a directory.", output_path.display()));
+                }
+            }
+            else {
+                self.output_path.push("trace.nettrace");
+            }
+        }
+
+        Ok(())
+    }
+
+    fn run(
+        &self,
+        machine: &mut ExportMachine,
+        _args: &RecordArgs) -> anyhow::Result<()> {
+
+        let _ = machine.to_net_trace(|_proc| { true }, &self.output_path.to_str().unwrap());
+
+        Ok(())
     }
 }
