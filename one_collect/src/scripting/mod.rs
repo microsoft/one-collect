91e4f7f5e2cd8b5ddf09dd9469db5ba7c3ba6b28
Beau Belgrave <beaub@linux.microsoft.com>
2025-07-18 15:44:35 -0700

    Scripting: Add OS version support
    Often times scripts need OS version details, such as which OS they are
    on (Windows, Linux, etc.) and what version (10.0, 6.11, etc.).
    
    Add ScriptEnvironment struct that allows fetching the scripting engine
    version and the OS version. Also move is_os() from it's own method to
    this environment to make it clear this is the object to use going
    forward.
    
    Add new_environment() method for scripts that returns the new
    ScriptEnvironment struct.
    
    Signed-off-by: Beau Belgrave <beaub@linux.microsoft.com>

--- one_collect/src/scripting/mod.rs
+++ one_collect/src/scripting/mod.rs
@@ -85,6 +85,68 @@ impl Clone for ScriptEvent {
     }
 }
 
+#[derive(Clone)]
+pub struct ScriptEnvironment {
+    os_major: u16,
+    os_minor: u16,
+}
+
+impl Default for ScriptEnvironment {
+    fn default() -> Self {
+        let (os_major, os_minor) = os::version();
+
+        Self {
+            os_major,
+            os_minor,
+        }
+    }
+}
+
+impl CustomType for ScriptEnvironment {
+    fn build(mut builder: TypeBuilder<Self>) {
+        builder
+            .with_fn("engine_major", Self::engine_major)
+            .with_fn("engine_minor", Self::engine_minor)
+            .with_fn("os_major", Self::os_major)
+            .with_fn("os_minor", Self::os_minor)
+            .with_fn("is_os", Self::is_os);
+    }
+}
+
+impl ScriptEnvironment {
+    pub fn is_os(
+        &mut self,
+        os: String) -> bool {
+        match os.as_str() {
+            "windows" => {
+                #[cfg(target_os = "windows")]
+                return true;
+
+                #[cfg(not(target_os = "windows"))]
+                return false;
+            },
+
+            "linux" => {
+                #[cfg(target_os = "linux")]
+                return true;
+
+                #[cfg(not(target_os = "linux"))]
+                return false;
+            },
+
+            _ => { false },
+        }
+    }
+
+    pub fn engine_major(&mut self) -> u16 { 1 }
+
+    pub fn engine_minor(&mut self) -> u16 { 0 }
+
+    pub fn os_major(&mut self) -> u16 { self.os_major }
+
+    pub fn os_minor(&mut self) -> u16 { self.os_minor }
+}
+
 pub struct ScriptEngine {
     engine: Engine,
     os: OSScriptEngine,
@@ -94,7 +156,12 @@ impl ScriptEngine {
     pub fn new() -> Self {
         let mut engine = Engine::new();
 
-        engine.build_type::<ScriptEvent>();
+        engine.
+            build_type::<ScriptEvent>().
+            build_type::<ScriptEnvironment>().
+            register_fn(
+                "new_environment",
+                || -> ScriptEnvironment { ScriptEnvironment::default() });
 
         Self {
             engine,
@@ -105,30 +172,6 @@ impl ScriptEngine {
     pub(crate) fn rhai_engine(&mut self) -> &mut Engine { &mut self.engine }
 
     pub fn enable_os_scripting(&mut self) {
-        self.engine.register_fn(
-            "is_os",
-            move |os: String| -> bool {
-                match os.as_str() {
-                    "windows" => {
-                        #[cfg(target_os = "windows")]
-                        return true;
-
-                        #[cfg(not(target_os = "windows"))]
-                        return false;
-                    },
-
-                    "linux" => {
-                        #[cfg(target_os = "linux")]
-                        return true;
-
-                        #[cfg(not(target_os = "linux"))]
-                        return false;
-                    },
-
-                    _ => { false },
-                }
-            });
-
         self.os.enable(&mut self.engine);
     }
 
@@ -165,7 +208,7 @@ mod tests {
 
         engine.enable_os_scripting();
 
-        let result = engine.eval::<bool>("is_os(\"windows\")").unwrap();
+        let result = engine.eval::<bool>("new_environment().is_os(\"windows\")").unwrap();
 
         #[cfg(target_os = "windows")]
         assert_eq!(true, result);
@@ -173,7 +216,7 @@ mod tests {
         #[cfg(target_os = "linux")]
         assert_eq!(false, result);
 
-        let result = engine.eval::<bool>("is_os(\"linux\")").unwrap();
+        let result = engine.eval::<bool>("new_environment().is_os(\"linux\")").unwrap();
 
         #[cfg(target_os = "windows")]
         assert_eq!(false, result);
