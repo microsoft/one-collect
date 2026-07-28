66240bc5cdc40497a70e2ab558db959994bff10e
Swapnil Ashtekar (from Dev Box) <swashtek@microsoft.com>
2026-07-28 15:48:33 -0700

    Refactor provider enumeration to use a closure for processing registered providers

--- one_collect/src/etw/mod.rs
+++ one_collect/src/etw/mod.rs
@@ -18,7 +18,7 @@ mod events;
 pub mod providers;
 pub mod tdh;
 
-pub use providers::{RegisteredProvider, registered_providers};
+pub use providers::{RegisteredProvider, for_each_registered_provider};
 
 use abi::{
     EVENT_RECORD,
